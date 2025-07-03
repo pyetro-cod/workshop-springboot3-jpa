c5c51a5b96fbe934071117c65469d96e9fde0623
Pyetro <pyetroribeirosantacruz@gmail.com>
2025-07-03 12:15:07 -0300

    Entidade Pedido. Datas com Instant e padrão ISO 8601 - PARTE 1

--- course/src/main/java/com/ppc/course/entities/User.java
+++ course/src/main/java/com/ppc/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.ppc.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 		
 	}
@@ -34,6 +40,11 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 
 	public long getId() {
 		return id;
@@ -91,6 +102,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return id == other.id;
 	}
+
 	
 	
 }
