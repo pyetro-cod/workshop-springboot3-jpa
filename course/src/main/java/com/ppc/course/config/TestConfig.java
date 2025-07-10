471e844672123bce648c0fa00cc8368a4df64c75
Pyetro <pyetroribeirosantacruz@gmail.com>
2025-07-10 00:12:21 -0300

    Entidade Category

--- course/src/main/java/com/ppc/course/config/TestConfig.java
+++ course/src/main/java/com/ppc/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.ppc.course.entities.Category;
 import com.ppc.course.entities.Order;
 import com.ppc.course.entities.User;
 import com.ppc.course.entities.enums.OrderStatus;
+import com.ppc.course.repositories.CategoryRepository;
 import com.ppc.course.repositories.OrderRepository;
 import com.ppc.course.repositories.UserRepository;
 
@@ -18,6 +20,9 @@ import com.ppc.course.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -27,6 +32,13 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
