a9c213bc669ef15aed59fd472dda761bc1e0553b
Pyetro <pyetroribeirosantacruz@gmail.com>
2025-07-22 23:06:31 -0300

    Tratamento de exceção - update

--- course/src/main/java/com/ppc/course/services/UserService.java
+++ course/src/main/java/com/ppc/course/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ppc.course.entities.User;
 import com.ppc.course.repositories.UserRepository;
+import com.ppc.course.services.exceptions.DatabaseException;
 import com.ppc.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -29,14 +34,26 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity,obj);
 		return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
+		
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
